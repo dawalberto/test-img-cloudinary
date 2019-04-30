2400c38b26fdf844e67a1190f66d5626f13f882b
Alberto Garcia <albertoinformatica97@gmail.com>
2019-04-30 22:06:10 +0200

    pruebas

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const app = require('express')()
+const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
+
+
+app.use( fileUpload({ useTempFiles: true }) )
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_MAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
+
 
 app.get('/', (req, res) => {
 
@@ -9,6 +21,27 @@ app.get('/', (req, res) => {
 
 })
 
-app.listen(process.env.PORT, () => {
-    console.log('listen on PORT', process.env.PORT)
+app.post('/upload-img', (req, res) => {
+
+    cloudinary.uploader.upload('js.png', {tags:'basic_sample'}, (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                image,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            result
+        })
+    })
+
+})
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('listen on PORT', port)
 })
